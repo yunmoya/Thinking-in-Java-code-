d3665e87992fded558b78320e7db46f03280f45a
LiCuican <li_cuican@163.com>
2018-08-05 18:58:43 +0800

    2018.8.5

---
+++ P31_ShowProperties/src/ShowProperties.java
@@ -0,0 +1,12 @@
+//: object/ShowProperties.java
+
+public class ShowProperties {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.getProperties().list(System.out);
+		System.out.println(System.getProperty("user.name"));
+		System.out.println(System.getProperty("java.library.path"));
+	}
+
+} ///:~
