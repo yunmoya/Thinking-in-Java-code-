d106b2a1cafa46b69776cbfe452777a14704fd22
LiCuican <li_cuican@163.com>
2018-08-06 14:05:24 +0800

    2018.8.6

---
+++ object/P35_HelloDate_comment/src/HelloDate.java
@@ -0,0 +1,22 @@
+//: object/HelloDate.java
+import java.util.*;
+
+/** The first Thinking in Java example program.
+ * Displays a string and today's date.
+ * @author Yunmo
+ * @version 4.0
+ */
+public class HelloDate {
+	/** Entry point to class & application. 
+	 * @param args array of string arguments
+	 * @throws exceptions No exceptions thrown
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello, it's: ");
+		System.out.println(new Date());
+	}
+
+}/* Output: (55% match)
+Hello, it's: 
+Mon Aug 06 13:59:56 CST 2018
+*///:~
