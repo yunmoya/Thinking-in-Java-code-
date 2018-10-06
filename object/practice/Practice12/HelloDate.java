fa6ac8076dfa0132e0f46432bbf354547eefe666
unknown <li_cuican@163.com>
2018-10-07 00:01:33 +0800

    practice

---
+++ object/practice/Practice12/HelloDate.java
@@ -0,0 +1,23 @@
+//: object/HelloDate.java
+import java.util.*;
+
+/** The first Thinking in Java example program.
+ * @author Bruce Eckel
+ * @author www.MindView.net
+ * @version 4.0
+ */
+public class HelloDate {
+	/** Entry point to class & application.
+	 * @param args array of string arguments
+	 * @throws exceptions No exceptions thrown
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello,it's: ");
+		System.out.println(new Date());
+	} /* Output: (55% match)
+	Hello,it's: 
+	Sat Sep 29 22:59:52 CST 2018
+	*///:~
+
+}
